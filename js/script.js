58e27fee2d0e9cd6ec006eef8be4b3c63126ad21
zahidhasan13 <zahidhasanofficial13@gmail.com>
2024-11-09 17:27:16 +0600

    responsive navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 "use strict";
+// Nav Toggle
+const navLinks = document.querySelector(".nav-links");
+const navToggle = document.querySelector(".mobile-nav-icon");
+
+navToggle.addEventListener("click", () => {
+  if (navLinks.classList.contains("nav-open")) {
+    navLinks.classList.remove("nav-open");
+    document.querySelector("html").style.overflow = "visible";
+  } else {
+    navLinks.classList.add("nav-open");
+    document.querySelector("html").style.overflow = "hidden";
+  }
+});
+
+navLinks.addEventListener("click", () => {
+  navLinks.classList.contains("nav-open") &&
+    navLinks.classList.remove("nav-open");
+  document.querySelector("html").style.overflow = "visible";
+});
+// Smooth behavior
+navLinks.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav-link")) {
+    const attr = e.target.getAttribute("href");
+    document.querySelector(attr).scrollIntoView({ behavior: "smooth" });
+  }
+});
